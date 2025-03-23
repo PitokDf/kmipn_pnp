84bf63a2405612b6060ef1a095107fe370f11ee6
Pito Desri Pauzi <pitokfauzi@gmail.com>
2025-03-23 09:05:23 +0700

    modified index.ts

--- web_kmipn_pnp/backend/src/index.ts
+++ web_kmipn_pnp/backend/src/index.ts
@@ -17,7 +17,7 @@ dotenv.config()
 
 const app = express();
 
-app.set('trust proxy', true)
+app.set('trust proxy', 1)
 app.use(helmet())
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(cookieParser());
